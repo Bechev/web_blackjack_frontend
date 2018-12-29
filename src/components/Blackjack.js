7258f797fd121ab3bef3b0d948a736bb1b85cfc3
bct <bertrand.chevalier@gmail.com>
2018-12-29 01:10:27 +0100

    Add win lose tie gameplay element and refined cards to include card symbols

--- src/components/Blackjack.js
+++ src/components/Blackjack.js
@@ -8,7 +8,7 @@ class Home extends Component {
   constructor(props){
     super(props)
     this.state = {
-      gameStatus: 'over',
+      gameStatus: 'not started',
       dealerHand: [],
       dealerPoints: 0,
       playerHand: [],
@@ -16,19 +16,21 @@ class Home extends Component {
     }
   }
 
+
+// ASYNC FUNCTIONS ///////////////////////////////////////////////////////////////////////
   deal(){
-    if (this.state.gameStatus === 'over'){
-      console.log('Deal Cards')
+    if (this.state.gameStatus === 'not started' || this.state.gameStatus === 'push' || this.state.gameStatus === 'lost' || 
+        this.state.gameStatus === 'busted' || this.state.gameStatus === 'won' || this.state.gameStatus === 'blackjack'){
       fetch('http://localhost:3000/hands/create')
       .then(response => {
         return response.json();
       })
       .then(function(data) {
-        if(parseInt(data.player_hand_value) >= 21){
+        if(parseInt(data.player_hand_value) === 21){
           this.setState({
-            gameStatus: 'over'
+            gameStatus: 'blackjack'
           })
-        }else{
+        }else {
           this.setState({
             gameStatus: 'ongoing'
           })
@@ -45,29 +47,24 @@ class Home extends Component {
   
   hit(){
     if (this.state.gameStatus === 'ongoing'){
-      console.log('Hit Cards')
     fetch('http://localhost:3000/hands/hit')
     .then(function(response) {
       return response.json();
     })
     .then(function(data) {
-      console.log(data)
-      if(parseInt(data.player_hand_value) >= 21){
-        this.setState({
-          gameStatus: 'over'
-        })
-      }
       this.setState({
         playerHand: data.player_hand,
         playerPoints: data.player_hand_value
       })
+      if(parseInt(data.player_hand_value)>=21){
+        this.stand()
+      }
     }.bind(this));
     }
-  }  
+  }
 
   stand(){
     if(this.state.gameStatus === 'ongoing'){
-      console.log('Stand Cards')
       fetch('http://localhost:3000/hands/stand')
       .then(function(response) {
         return response.json();
@@ -78,11 +75,35 @@ class Home extends Component {
           dealerPoints: data.dealer_hand_value,
           playerHand: data.player_hand,
           playerPoints: data.player_hand_value,
-          gameStatus: 'over'
         })
+        if(parseInt(data.player_hand_value) > 21){
+          this.setState({
+            gameStatus: 'busted'
+          })
+        }else if(parseInt(data.dealer_hand_value) > 21){
+          this.setState({
+            gameStatus: 'won'
+          })
+        } else {
+          if(parseInt(data.dealer_hand_value) > parseInt(data.player_hand_value)){
+            this.setState({
+              gameStatus: 'lost'
+            })
+          }else if (parseInt(data.dealer_hand_value) === parseInt(data.player_hand_value)){
+            this.setState({
+              gameStatus: 'push'
+            })
+          }else{
+            this.setState({
+              gameStatus: 'won'
+            })
+          }
+        }
       }.bind(this));
     }
   }  
+// END OF ASYNC FUNCTIONS //////////////////////////////////////////////////////////
+
 
 // TO BE REMOVED //////////////
   log_state(){
@@ -90,20 +111,19 @@ class Home extends Component {
   }
 ///////////////////////////////  
 
+// RENDER FUNCTIONS ///////////////////////////////////////////////////////////////////
   renderHand(hand){
-    console.log(hand)
     if (hand.length === 0){
       return(
         <span className='card-container'>
-          <Card card={"  "} flipped={false}/>
-          <Card card={"  "} flipped={false}/>
+          <Card flipped={false}/>
+          <Card flipped={false}/>
         </span>
       )
     } 
    else {
       return(
         hand.map(card => {
-          console.log(card)
           return(
             <span>
               <Card card={card} flipped={true}/>
@@ -130,42 +150,73 @@ class Home extends Component {
     }
   }
 
+  renderResult(){
+    let results
+    if( this.state.gameStatus === 'lost' || this.state.gameStatus === 'busted'){
+      results = 'lostGame'
+    }else if(this.state.gameStatus === 'won' || this.state.gameStatus === 'blackjack'){
+      results = 'wonGame'
+    } else if(this.state.gameStatus === 'push'){
+      results = 'tieGame'
+    }
+    return results
+  }
+
+  // renderGameStatus(){
+  //   if (this.state.gameStatus === 'lost' ||
+  //       this.state.gameStatus === 'busted' ||
+  //       this.state.gameStatus === 'won'||
+  //       this.state.gameStatus === 'blackjack'){
+
+  //         return(
+  //           <div>
+  //             {this.state.gameStatus}
+  //           </div>
+  //         )
+  //       }
+  // }
+// END OF RENDER FUNCTIONS ///////////////////////////////////////////////////////////////////
     render() {
       let drawIsActive = true
       let hitIsActive = false
       let standIsActive = false
 
-      if(this.state.gameStatus == 'over'){
+      if(this.state.gameStatus === 'lost'){
         
-      } else if(this.state.gameStatus == 'ongoing') { 
+      } else if(this.state.gameStatus === 'ongoing') { 
         drawIsActive = false
         hitIsActive = true
         standIsActive = true
       }
 
-      // let results=""
-      // if (this.state.gameStatus == 'over'){
-      //   results += "result"
-      // }
+
     
     return (
       <div className='background'>   
-        <div className= 'dealerHand'>
-          {this.renderPoints('dealer')}
-          {this.renderHand(this.state.dealerHand)}
-        </div>
-        <div className='playerHand'>
-          {this.renderPoints('player')}
-          <div className={results}>
-            {this.renderHand(this.state.playerHand)}
+        <span>
+
+          <div className= 'dealerHand'>
+            {this.renderPoints('dealer')}
+            {this.renderHand(this.state.dealerHand)}
           </div>
-        </div>
+
+          <div className='playerHand'>
+            {this.renderPoints('player')}
+
+            {/* {this.renderGameStatus()} */}
+            <div className={this.renderResult()}>
+              {this.renderHand(this.state.playerHand)}
+            </div>
+          </div>
+
+        </span>
         <div className= 'dashboard'>
           <Button name='Draw' handleClick={this.deal.bind(this)} isActive={drawIsActive}></Button>
           <Button name='Hit' handleClick={this.hit.bind(this)} isActive={hitIsActive}></Button>
           <Button name='Stand' handleClick={this.stand.bind(this)} isActive={standIsActive}></Button>
           <Button name='Log state' handleClick={this.log_state.bind(this)} isActive={true}></Button>
         </div>
+
       </div>
     );
   }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,33 +1,41 @@
 import React, { Component } from 'react';
 import './blackjack.css'
+import hearts from '../assets/cards_hearts.jpg'
+import diamonds from '../assets/cards_diamonds.jpg'
+import spades from '../assets/cards_spades.jpg'
+import clubs from '../assets/cards_clubs.jpg'
 import './card.css';
 
 class Card extends Component {
 
-  constructor(props){
-    super(props)
-    this.state = {
-        flipped: this.props.flipped
-    }
-  }
-
-  handleClick =(event) => {
-      event.preventDefault()
-      this.setState({
-          flipped: !this.state.flipped
-        })
-        console.log(this.state.flipped)
-  }
-
-
     renderCard(){
-        if (this.state.flipped === true){
+        if (this.props.flipped === true){
+            let card_symbol = null
+            switch(this.props.card[this.props.card.length -1]){
+                case 'h':
+                    card_symbol = hearts
+                    break
+                case 's':
+                    card_symbol = spades
+                    break
+                case 'c':
+                    card_symbol = clubs
+                    break
+                case 'd':
+                    card_symbol = diamonds
+                    break
+            }
             return (
-                <div className="card flippedCard">{this.props.card.slice(0,-1)}</div>
+                <div className="card flippedCard">
+                    {this.props.card.slice(0,-1)}
+                    <span>
+                        <img className="card-symbol" src={card_symbol} alt='card' height="25" width="25"></img>
+                    </span>
+                </div>
             )
-        }else if (this.state.flipped === false){
+        }else if (this.props.flipped === false){
             return (
-                <div className="card unflippedCard"> </div>
+                <div className="card unflippedCard"></div>
             )
         }
     }
